05b08cfe15a39c6c4d68a54f334ed3ca1ba3d1b3
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-08-08 20:41:42 -0600

    Added euclidean distance

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use std::f64;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,10 +7,27 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn distance(p: Vec<f64>, q: Vec<f64>) -> PyResult<f64> {
+    if p.len() != q.len() {
+        return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            "Error: Arrays must always have the same size."
+        ));
+    }
+
+    let squares_sum = p.iter()
+        .zip(q.iter())
+        .map(|(a, b)| (a - b).powi(2))
+        .sum::<f64>();
+
+    Ok(squares_sum.sqrt())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn stmeasures(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(distance, m)?)?;
 
     Ok(())
 }
